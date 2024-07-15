6d70faf850691d9548c36cbbfa1696389a81e149
Wisam Ahmad <wisamahmad786@gmail.com>
2024-07-15 15:40:16 +0500

    implement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.ts";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
