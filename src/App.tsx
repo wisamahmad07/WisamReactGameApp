0d4757d377ceed3e3162dc6f256fe872544c6071
Wisam Ahmad <wisamahmad786@gmail.com>
2024-07-15 15:36:44 +0500

    created till navbar

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <Navbar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="green">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
