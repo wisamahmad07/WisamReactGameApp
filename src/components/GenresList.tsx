a611ad56f6f5c14eeead6b8f62e6882ed3f15d6d
Wisam Ahmad <wisamahmad786@gmail.com>
2024-07-16 18:40:01 +0500

    display genres with bg-image and fix layout of gamecards by removing width="300px" and add template columns

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,12 +1,22 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import CropImage from "../services/image-url";
 
 const GenresList = () => {
   const { data } = useGenres();
   return (
-    <List>
+    <List padding={3} marginY={7}>
       {data.map((genre) => (
-        <ListItem>{genre.name}</ListItem>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={CropImage(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
     </List>
   );
