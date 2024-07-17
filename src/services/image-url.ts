f79f887983139bb54f838b932875b329ec3b495d
Wisam Ahmad <wisamahmad786@gmail.com>
2024-07-17 22:28:16 +0500

    handle no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noimage from "../assets/no-image-placeholder.webp";
 const CropImage = (url: string) => {
-  if (!url) return "";
+  if (!url) return noimage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
