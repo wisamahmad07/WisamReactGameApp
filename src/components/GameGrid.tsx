db1987b21500f4d40824f7793720acf9442cb002
Wisam Ahmad <wisamahmad786@gmail.com>
2024-07-18 12:57:07 +0500

    refactor game grid and "FINAL COMMIT"

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,28 +13,26 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { error, data, loading } = useGames(gameQuery);
   const skeletonsNumber = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text color="red">{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={5}
-        paddingX={5}
-        marginTop={3}
-      >
-        {loading &&
-          skeletonsNumber.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <LoadingSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={5}
+      paddingX={5}
+      marginTop={3}
+    >
+      {loading &&
+        skeletonsNumber.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <LoadingSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
